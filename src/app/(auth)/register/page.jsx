7b598b11930f836c85d1ab2839fa0cf3857ca86d
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-04-27 16:30:07 +0600

    Implement User Registration page with form handling and password visibility toggle

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-const UserLogin = () => {
+const UserRegisterPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const {
     register,
@@ -21,36 +21,8 @@ const UserLogin = () => {
     <div className="min-h-screen w-full flex items-center justify-center p-4 md:p-8">
       {/* login middle div */}
       <div className="flex w-full max-w-300 min-h-200 bg-white rounded-[40px] overflow-hidden shadow-2xl">
-        {/* left side Image visual and quote */}
-        <div className="hidden lg:flex relative w-1/2 p-16 flex-col justify-between overflow-hidden bg-black text-white">
-          {/* BG Image Wrapper */}
-          <Image
-            src="/images/chic-empty-room-authentic-interior-design.jpg"
-            alt="Metora Decor Login Page"
-            fill
-            className="object-cover opacity-80"
-            priority
-          />
-          {/* Content OverLay */}
-          <div className="relative z-10">
-            <div className="flex items-center gap-4">
-              <span className="text-xs font-bold tracking-[0.2rem] uppercase opacity-70">
-                A Wise Quote
-              </span>
-              <div className="h-px w-12 bg-white opacity-30 "></div>
-            </div>
-          </div>
-          <div className="relative z-10 max-w-md">
-            <h1 className="text-7xl font-[family-name:var(--font-aurore)] leading-tight mb-6">
-              Get Everything You Want
-            </h1>
-            <p className="text-lg opacity-80 font-[family-name:var(--font-mont)] leading-relaxed">
-              You can get everything you want if you work hard, trust the
-              process, and stick to the plan.
-            </p>
-          </div>
-        </div>
-        {/* Right Side main form */}
+        {/* left  Side main form*/}
+
         <div className="w-full lg:w-1/2 p-8 flex flex-col justify-between bg-white">
           {/* logo */}
           <div className="flex items-center justify-center mb-8">
@@ -131,7 +103,7 @@ const UserLogin = () => {
                 Login
               </button>
 
-             {/* Social Media Button */}
+              {/* Social Media Button */}
             </div>
           </form>
           {/* Footer */}
@@ -145,9 +117,39 @@ const UserLogin = () => {
             </Link>
           </p>
         </div>
+        {/* Right side Image visual and quote */}
+
+        <div className="hidden lg:flex relative w-1/2 p-16 flex-col justify-between overflow-hidden bg-black text-white">
+          {/* BG Image Wrapper */}
+          <Image
+            src="/images/chic-empty-room-authentic-interior-design.jpg"
+            alt="Metora Decor Login Page"
+            fill
+            className="object-cover opacity-80"
+            priority
+          />
+          {/* Content OverLay */}
+          <div className="relative z-10">
+            <div className="flex items-center gap-4">
+              <span className="text-xs font-bold tracking-[0.2rem] uppercase opacity-70">
+                A Wise Quote
+              </span>
+              <div className="h-px w-12 bg-white opacity-30 "></div>
+            </div>
+          </div>
+          <div className="relative z-10 max-w-md">
+            <h1 className="text-7xl font-[family-name:var(--font-aurore)] leading-tight mb-6">
+              Get Everything You Want
+            </h1>
+            <p className="text-lg opacity-80 font-[family-name:var(--font-mont)] leading-relaxed">
+              You can get everything you want if you work hard, trust the
+              process, and stick to the plan.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default UserLogin;
+export default UserRegisterPage;
